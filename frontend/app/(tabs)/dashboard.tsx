904bdfcffca9875c67290c5619c56a537bff3da5
yousufmohi <yousufmohiuddin0529@gmail.com>
2025-04-30 20:36:32 -0400

    init setup

---
+++ frontend/app/(tabs)/dashboard.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '@/components/EditScreenInfo';
+import { Text, View } from '@/components/Themed';
+
+export default function TabOneScreen() {
+  return (
+    <View >
+      <Text>Project Islam</Text>
+      <View lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    </View>
+  );
+}
+
+//popup on hold map
